5a617a3887492b81f5beed3b3c48d173c0759922
reich452 <reich452@regis.edu>
2017-03-29 17:13:54 -0600

    Added constraints

---
+++ VerticalCalculator/LabelKeys.swift
@@ -0,0 +1,13 @@
+//
+//  LabelKeys.swift
+//  VerticalCalculator
+//
+//  Created by Nick Reichard on 3/29/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import Foundation
+
+struct Labels {
+    let vertText = "Vert"
+}
--- VerticalCalculator/SnowbirdViewController.swift
+++ VerticalCalculator/SnowbirdViewController.swift
@@ -12,6 +12,8 @@ class SnowbirdViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpButton()
+        setUpButtonConstraints()
     }
     
     // MARK: - Labels
@@ -25,20 +27,41 @@ class SnowbirdViewController: UIViewController {
     
     // MARK: - Button
     
-    let plussButton = UIButton()
+    let plusButton = UIButton()
     
     // MARK: - SetUp Methods
     
     
     func setUpButton() {
         
+        plusButton.setImage(#imageLiteral(resourceName: "PlussButton"), for: .normal)
+        
+        // Add targets
+        plusButton.addTarget(self, action: #selector(plussButtonTapped), for: .touchUpInside)
+        
+        
+        // Add buttons as subviews of the ViewController's view
+        self.view.addSubview(plusButton)
+        
     }
     
     func setUpButtonConstraints() {
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Height and Width constraints
+        let topButtonWidths = NSLayoutConstraint(item: plusButton, attribute: .width, relatedBy: .equal, toItem: self.view, attribute: .width, multiplier: 0.18, constant: 0)
+        let plusButtonHeight = NSLayoutConstraint(item: plusButton, attribute: .height, relatedBy: .equal, toItem: plusButton, attribute: .width, multiplier: 1, constant: 0)
+        let plusButtonBottom = NSLayoutConstraint(item: plusButton, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: -50)
+        let plusButtonCenterX = NSLayoutConstraint(item: plusButton, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0)
+    
+        self.view.addConstraints([topButtonWidths, plusButtonBottom, plusButtonHeight, plusButtonCenterX])
         
     }
     
     func setUpLables() {
+       // vertLabel.frame = CGRect(x: 50, y: 80, width: 200, height: 30)
+        
+        
         
     }
     
@@ -54,8 +77,9 @@ class SnowbirdViewController: UIViewController {
         
     }
     
-    /// Test this git push 
+    func plussButtonTapped() {
+        
+    }
     
     
-
 }
